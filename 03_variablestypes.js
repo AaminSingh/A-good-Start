4fcf46a000a62340f7ff0666697ab223c3b94112
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2024-09-15 17:07:48 +0530

    converson if datatypes

---
+++ 03_variablestypes.js
@@ -0,0 +1,33 @@
+/*let score = "true"
+
+console.log(typeof score);
+console.log(typeof(score));//but I have to perform operstions on string
+
+##let valueInNumber = Number(score)//it guarantee that the score has been converted into number
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+
+//don't rely on numbers
+//NaN =>not a number    true =>1,  false=>0*/
+ 
+
+/*let isLoggedIn = "sumi"
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);*/
+
+
+
+
+//1=> true, 0=>false
+//""=>false, "sumi"=>true
+
+
+
+let someNumber = "78"
+
+let stringNumber = String(someNumber);
+console.log(typeof(stringNumber));
+console.log( stringNumber);
+
