6fbd2bfcf2c3e71c52efb2c1090f5812f83516cd
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2024-09-15 16:18:32 +0530

    new update

---
+++ 01_variables.js
@@ -0,0 +1,11 @@
+const accountId =145677
+let accountEmail = "a8@gmail.com"
+var accountPassword = "678897"
+accountCity = "delhi"
+let acountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+/*please not to use var
+because of the issue in block scope and functional scope*/
---
+++ 02_variables.js
@@ -0,0 +1,21 @@
+ "user strict";// treat all js code as newer version
+ //alert ("hello") //we are using node js not browser
+ console.log("aamin");
+ console.log(9+8);//code readibilty should be high
+
+ let name ="aamin";
+ let age = 19;
+ let isLoggedIn = true
+
+
+ //number =2 to the power 53
+ //bigint =>
+    //string=""
+ //boolean = true/false
+ //nuu =>stand alone value
+ //undefined =>value not assigned yet 
+ //symbol =>to find uniqueness
+ //object 
+ 
+ console.log(typeof null);//object
+ console.log(typeof undefined);
