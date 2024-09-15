8591cad4a7f23db6c642ef95f6b2e627afd807b9
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2024-09-15 18:30:14 +0530

    adding variable types

--- 03_variablestypes.js
+++ 03_variablestypes.js
@@ -25,9 +25,56 @@ console.log(booleanIsLoggedIn);*/
 
 
 
-let someNumber = "78"
+/*let someNumber = "78"
 
 let stringNumber = String(someNumber);
 console.log(typeof(stringNumber));
-console.log( stringNumber);
+console.log( stringNumber);*/
+
+
+//==================================OPERATIONS======================================
+
+let value =3
+let negValue =-value
+//console.log(negValue);
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%3);*/
+
+/*let str1 ="hello"
+let str2 =" hitesh"
+
+let str3 = str1+str2
+console.log(str3);
+console.log(1+"2");
+console.log("1"+2);// with this type code will not be reviewed
+
+console.log(1+2+"2");*/
+
+
+
+//console.log(+true);// doen't makes sense
+//console.log(+"");
+
+let num1,num2,num3
+
+//num1=num2=num3 =2+2// might be smart work but not readible for the rest of the members
+
+let gameCounter = 200
+gameCounter++;
+console.log(gameCounter);
+
+
+ //prefix=> value increment  earlier
+ //postfix=> value increment after
+
+
+
+
+
+
+
 
