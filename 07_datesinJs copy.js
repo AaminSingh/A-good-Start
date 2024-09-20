3c70380deace14c34bb213bc8de123872561652d
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2024-09-20 19:19:27 +0530

    array_01.js

--- 07_datesinJs copy.js
+++ 07_datesinJs copy.js
@@ -2,17 +2,19 @@
 
 let myDate = new Date()
 // console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+// console.log(myDate.toDateString());  //here it includes the date  alongwith the day
+// console.log(myDate.toLocaleString());  // here it includes the time alongwith the date
+// console.log(typeof myDate);  // it shows that type of date is object 
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toLocaleString()); //gives us the exact date of the input we put in the value
+// ususally gives the location as well 
 
-let myTimeStamp = Date.now()
+
+let myTimeStamp = Date.now()  //especially used to deign qizzes and poles
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
