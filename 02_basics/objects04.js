1c9105d181080ed81986d594ca65ab7f913ac95f
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2024-09-26 12:42:14 +0530

    objects04

--- 02_basics/objects04.js
+++ 02_basics/objects04.js
@@ -1,5 +1,8 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object() // this is a singleton object (using Object constructor fxn)
+const tinderUser = {} // this is not a singleton object 
+
+// A singleton pattern in JavaScript requires additional logic to ensure that only one instance of an object exists.
+
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -7,8 +10,9 @@ tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
 
+// objects inside objects
 const regularUser = {
-    email: "some@gmail.com",
+    email: "some@gmailcom",
     fullname: {
         userfullname: {
             firstname: "hitesh",
@@ -17,43 +21,49 @@ const regularUser = {
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname); // to access deep rooted values of objects
 
-
+const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4) // combines from one or more source to a targeted object 
+// const obj3 = { obj1, obj2 } // same problem as in array.. object inside object
+// const obj3 = Object.assign({}, obj1, obj2, obj4)  // returns a modified target object and is stored inside a new var.. 
+// {} is given to define the target and obj1,obj2,obj3 are inserted inside {}
+// not compulsory to give {}; in its absence, all the values will go inside obj1
 
+// spread method is best 
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
-
+// when data comes from database, it is in form of array 
 const users = [
     {
         id: 1,
         email: "h@gmail.com"
     },
     {
-        id: 1,
-        email: "h@gmail.com"
+        id: 2,
+        email: "f@gmail.com"
     },
     {
-        id: 1,
-        email: "h@gmail.com"
+        id: 3,
+        email: "g@gmail.com"
     },
 ]
 
-users[1].email
+users[1].email // to access the property of the object inside an array
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser)); // to access all the values form the keys and put in the array
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser)); // Object.keys() gives the keys of the object and returns in array
+// console.log(Object.values(tinderUser)); // returns values inside an array
+// console.log(Object.entries(tinderUser)); // every key value pair is converted in an array with keys & values as elements of that array. the whole object is the main array 
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn')); //useful for checking that whether they contain such property or not
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); // to check if a certain property is inside the object 
 
+
+
+// object destructure 
 
 const course = {
     coursename: "js in hindi",
@@ -61,19 +71,28 @@ const course = {
     courseInstructor: "hitesh"
 }
 
-// course.courseInstructor
+// course.courseInstructor // no problem with this way..  but..
 
-const {courseInstructor: instructor} = course
+const {courseInstructor: instructor} = course // this is also how to destructure and get the value. 
+// const{courseInstructor} = course 
 
 // console.log(courseInstructor);
 console.log(instructor);
 
+
+// APIs
+
 // {
 //     "name": "hitesh",
-//     "coursename": "js in hindi",  // it will be treated like a json
+//     "coursename": "js in hindi",
 //     "price": "free"
-// }
+// } 
+// this looks like an object but it is not cz it has no name and also : is used. it's JSON
+// means it is almost treated as a json 
+// in proper json, the keys and values are all strings g
 
+
+// sometimes APIs are received in the form of arrays including objects 
 [
     {},
     {},
