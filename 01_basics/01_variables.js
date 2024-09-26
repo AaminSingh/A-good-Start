848442aa67548bcf80b0503d3a97431f7b1f90d7
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2024-09-26 11:54:17 +0530

    variables_01

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,11 +1,23 @@
-const accountId =145677
-let accountEmail = "a8@gmail.com"
-var accountPassword = "678897"
-accountCity = "delhi"
-let acountState;
+const accountId = 144553 // this is constant and can't be changed
+let accountEmail = "officialvanshdixit@gmail.com"
+var accountPass = "12345"
+accountCity = "Lucknow"
+let accountState;
+/*
+accountId = 2 // trying to change but it can't be changed
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+*/
 
+accountEmail = "vanshdixit2005@gmail.com"
+accountPass = "2355ak"
+accountCity = "Tokyo"
 
-/*please not to use var
-because of the issue in block scope and functional scope*/
+// better to display all of these in table instead of writing console.log() for every variable
+console.table([accountId, accountEmail, accountPass, accountCity]); // indices are in numeral format
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]); // accountState is undefined
+console.table({accountId: accountId, accountEmail: accountEmail}); // indices are variables and values are values of the respective variable
+
+/* there are two ways to declare variables using "let" and also "var"
+but prefer not to use var
+because of isssues  in block scope and functional scope
+*/
